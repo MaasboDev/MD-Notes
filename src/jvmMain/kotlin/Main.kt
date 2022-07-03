33afa8303368d972d2d7da80c893f43db319fd4b
Marçal <marcal.asbo@gmail.com>
2022-07-03 11:12:26 +0200

    Módulo 2

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,32 +1,75 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
+
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
+class AppState {
+    val notes = mutableStateOf(getNotes())
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+@Composable
+@Preview
+fun App(appState: AppState) {
+
+    MaterialTheme {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(appState.notes.value) { note ->
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(0.8f)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Row {
+                            Text(
+                                text = note.title,
+                                style = MaterialTheme.typography.h6,
+                                modifier = Modifier.weight(1f)
+                            )
+
+                            if (note.type == Note.Type.AUDIO)
+                                Icon(
+                                    imageVector = Icons.Default.Mic,
+                                    contentDescription = null
+                                )
+
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(note.description)
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+fun main() {
+    val appState = AppState()
+
+    application {
+        Window(onCloseRequest = ::exitApplication) {
+            App(appState)
+        }
     }
 }
